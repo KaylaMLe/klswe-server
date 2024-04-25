177566206ad6755d20728cf14fa52e03fa471447
KaylaMLe <kaylale2014@gmail.com>
2024-04-25 01:25:07 -0700

    deleted tutorial site

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -5,7 +5,6 @@ from django.urls import include, path
 
 urlpatterns = [
 	path("admin/", admin.site.urls),
-	path("polls/", include("polls.urls")),
 	path("translate/", include("translate.urls")),
 ]
 
