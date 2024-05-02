ab49252d36ef2686627631c2f3d62ed264c973db
Kayla Le <kaylale2014@gmail.com>
2024-05-02 03:28:04 +0000

    fixed server-side gcloud authentication

--- mysite/translate/gemini/translate.py
+++ mysite/translate/gemini/translate.py
@@ -7,3 +7,9 @@ def translate(code: str) -> HttpResponse:
 	typescript_translation = model.generate_content(code)
 
 	return HttpResponse(typescript_translation.text)
+
+# generation test
+if __name__ == "__main__":
+    model = genai.GenerativeModel(model_name="tunedModels/js-to-ts-model-001")
+    translation = model.generate_content("foobar")
+    print(translation.text)
