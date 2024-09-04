dc4db3b167aee4ee1f5f22c022c7bdd4f5e78c8d
KaylaMLe <kaylale2014@gmail.com>
2024-09-03 20:20:00 -0700

    added test for status and cookie presence

--- mysite/csrf_setter/tests.py
+++ mysite/csrf_setter/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class SetCSRFCookieViewTest(TestCase):
+	def setUp(self):
+		self.client = Client()
+
+	def test_set_csrf_cookie(self):
+		response = self.client.get(reverse('csrf-setter:set_csrf_cookie'), secure=True)
+		self.assertEqual(response.status_code, 204)
+		self.assertIn('csrftoken', response.cookies)
