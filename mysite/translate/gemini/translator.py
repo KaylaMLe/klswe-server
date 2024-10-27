c42d7379049e81a1a4212da838d6d1f5eb078083
KaylaMLe <kaylale2014@gmail.com>
2024-10-27 14:16:34 -0700

    fixed api key usage

--- mysite/translate/gemini/translator.py
+++ mysite/translate/gemini/translator.py
@@ -8,7 +8,13 @@ load_dotenv()
 
 class Translator:
 	def __init__(self):
-		self.model = genai.GenerativeModel(model_name="tunedModels/" + os.environ["ACTIVE_MODEL_ID"])
+		genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
+		for model_info in genai.list_tuned_models():
+			print("*")
+			print(model_info.name)
+
+		self.model = genai.GenerativeModel("gemini-1.5-flash")
 
 	def translate(self, code: str) -> str:
 		typescript_translation = self.model.generate_content(code)
--- mysite/translate/views.py
+++ mysite/translate/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpRequest, HttpResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.decorators.http import require_http_methods
-from utils.shared_utils import validate_code_request
+from utils.shared_utils import preflight_handler, validate_code_request
 from .gemini.translator import Translator
 
 
@@ -10,21 +10,13 @@ model = Translator()
 
 @ensure_csrf_cookie
 @require_http_methods(["GET", "POST"])
+@preflight_handler()
 def translate_code(request: HttpRequest) -> HttpResponse:
-	if request.method == "POST":
-		code_or_error = validate_code_request(request)
+	code_or_error = validate_code_request(request)
 
-		if isinstance(code_or_error, HttpResponse):
-			return code_or_error
+	if isinstance(code_or_error, HttpResponse):
+		return code_or_error
 
-		translation = model.translate(code_or_error)
-		
-		return HttpResponse(translation)
-	elif request.method == "GET":
-		return HttpResponse()
-	else:
-		return HttpResponse(
-			f"Expected GET or POST request but got a {request.method} request.",
-			status=405
-		)
+	translation = model.translate(code_or_error)
 
+	return HttpResponse(translation)
