94a84d03d012516d8840fff58f2647e624c88ec6
KaylaMLe <kaylale2014@gmail.com>
2024-11-01 19:41:34 -0700

    added dev mode check to choose the appropriate authentication method

--- mysite/translate/gemini/translator.py
+++ mysite/translate/gemini/translator.py
@@ -1,3 +1,5 @@
+from google.auth import load_credentials_from_file
+import google.auth.transport.requests
 from dotenv import load_dotenv
 import os
 import vertexai
@@ -8,12 +10,28 @@ from .system_instruction import system_instruction
 
 load_dotenv()
 
-
 class Translator:
 	def __init__(self):
-		vertexai.init(project=os.environ["PROJECT_ID"], location=os.environ["REGION"])
+		# if dev mode is on, use the application default credentials
+		credentials = None
+		# if dev mode is off (i.e. in production), use the service account credentials
+		if os.environ["DEV_MODE"] != "True":
+			credentials, _ = load_credentials_from_file(os.environ["CREDENTIALS_PATH"])
+			credentials = credentials.with_scopes(["https://www.googleapis.com/auth/cloud-platform"])
 
-		self.model = GenerativeModel(model_name=os.environ["MODEL_ID"], system_instruction=system_instruction)
+			# request an access token for the GCP service account
+			request = google.auth.transport.requests.Request()
+			credentials.refresh(request)
+
+		vertexai.init(
+			credentials=credentials,
+			project=os.environ["PROJECT_ID"],
+			location=os.environ["REGION"]
+		)
+		self.model = GenerativeModel(
+			model_name=os.environ["MODEL_ID"],
+			system_instruction=system_instruction
+		)
 
 	def translate(self, code: str) -> str:
 		typescript_translation = self.model.generate_content(code)
