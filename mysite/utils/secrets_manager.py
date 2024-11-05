7a336172f9f45368391108b16a583d51f75a31b1
KaylaMLe <kaylale2014@gmail.com>
2024-11-04 21:09:47 -0800

    improved error logging

--- mysite/utils/secrets_manager.py
+++ mysite/utils/secrets_manager.py
@@ -1,8 +1,10 @@
 import boto3
 from botocore.exceptions import ClientError
+from datetime import datetime
 from dotenv import load_dotenv
 import json
 import os
+import traceback
 
 
 load_dotenv()
@@ -29,11 +31,21 @@ class SecretsManager:
 			return cls._secrets_cache[secret_name]
 
 		client = cls.get_client()
-		
+
 		try:
 			get_secret_value_response = client.get_secret_value(SecretId=secret_name)
-		except ClientError as e:
-			raise e
+		except ClientError:
+			current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+			log_filename = f"error_log_{current_time}.txt"
+
+			# write to a file to avoid printing sensitive information to the console
+			with open(os.path.join(os.environ["LOG_PATH"], log_filename), "a") as f:
+				f.write(f"{traceback.format_exc()}\n")
+
+			raise RuntimeError(
+				f"A ClientError occurred while retrieving secret {secret_name}. "
+				"Check {log_filename} for more details."
+			)
 
 		# decrypt secret using the associated KMS key and store it in cache
 		secret = json.loads(get_secret_value_response['SecretString'])
